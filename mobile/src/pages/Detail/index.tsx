a6ad4828efd84868f057ac2898963ab9184325d4
alexmanto <40876399+alexmanto@users.noreply.github.com>
2020-06-17 22:55:53 -0300

    Finalização da busca dos detalhes do ponto na api

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, SafeAreaView, Linking } from 'react-native';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import { useNavigation, useRoute, useFocusEffect } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import * as MailComposer from 'expo-mail-composer';
 import api from '../../services/api';
 
 interface Params {
@@ -39,6 +40,17 @@ const Detail = () => {
         navigation.goBack();
     }
     
+    function handleComposeMail() { 
+      MailComposer.composeAsync({ 
+        subject : 'Interesse na coleta de resíduos',
+        recipients: [data.point.email]
+      });
+    }
+
+    function handleWhatsapp() {
+      Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos.`)
+    }
+
     if (!data.point) {
       return null;
     }
@@ -60,12 +72,12 @@ const Detail = () => {
                 </View>
             </View>
             <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleWhatsapp}>
                     <FontAwesome name="whatsapp" size={20} colot="#FFF" />
                     <Text style={styles.buttonText}>Whatsapp</Text>
                 </RectButton>
 
-                <RectButton style={styles.button} onPress={() => {}}>
+                <RectButton style={styles.button} onPress={handleComposeMail}>
                     <Icon name="mail" size={20} colot="#FFF" />
                     <Text style={styles.buttonText}>E-mail</Text>
                 </RectButton>
